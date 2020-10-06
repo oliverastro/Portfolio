bb80dbc51cba0a822e5bfc8b847773592e500100
oliverastro <oliverastro@gmail.com>
2020-10-05 23:51:35 -0300

    - Added favicon - Added Typeform - Added Tooltip - Added Smooth Scrolling - Changed styles CSS - Changed structure HTML

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,41 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip();
+// $(function () {
+//     $('[data-toggle="tooltip"]').tooltip();
 
-    $('.carousel').carousel();
+//     $('.carousel').carousel();
 
-  })
+// })
+
+$(function() {
+
+  $('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+  if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+  ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: target.offset().top
+      }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { 
+          return false;
+          } else {
+          $target.attr('tabindex','-1');
+          $target.focus();
+          };
+      });
+      }
+  }
+  });
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+})
